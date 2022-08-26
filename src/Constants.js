2a46e7da2293728213004e1a8041cd56bb45b73c
Kaustuv942 <kaustuv942@gmail.com>
2022-08-26 13:49:49 +0530

    refEasy Body

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,9 @@
-const MAIL_INVITE_SUBJECT = 'RetireEasy Invitation';
+const MAIL_INVITE_SUBJECT = 'REFEASY Invitation';
 const MAIL_INVITE_TEXT = 'Hi <br> Take a look at exciting opportunity '
 const MAIL_VACANCY_ADMIN_REMINDER_SUBJECT = 'Hey Total number of pending vacancies '
 const MAIL_VACANCY_ADMIN_REMINDER_TEXT = 'Hey Total number of pending vacancies '
+const MAIL_SUBSCRIBED_SUBJECT = 'Hello, We are excited to let you know a position that you subscribed to on our application  RetireEasy has opened up. We would be delighted to see you apply to it.  For further details : Login to your RetireEasy Account: https://faculty-vacancy.netlify.app/login and check vacancies and your subscriptions for further processes.Thanking you,Admin'
+const MAIL_SUBSCRIBED_TEXT = 'REFEASY Invitation'
 
 module.exports = {
     MAIL_INVITE_SUBJECT,
